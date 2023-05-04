6bb1684bb1c7b2804ec9c436fb17e48e6fa9c21c
Lornarysco <badgers.barsukoff2013@gmail.com>
2023-05-05 01:59:21 +0400

    123

---
+++ front/src/pages/UserInfo/Userinfo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Userinfo.scss';
+
+function Userinfo() {
+  return (
+    <div className="Main-02">
+      Userinfo
+    </div>
+  );
+}
+
+export default Userinfo;
