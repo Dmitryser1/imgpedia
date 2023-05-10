0a5fa89b4e6cb482bc821cf3a9331a12978f7048
Dmitryser1 <dmitryserafimovich@gmail.com>
2023-05-10 17:12:16 +0400

    2

--- front/src/components/stories/Stories.jsx
+++ front/src/components/stories/Stories.jsx
@@ -51,6 +51,18 @@ function FormRow() {
           alt=""
         /></Item>
       </Grid>
+      <Grid item xs={'auto'}>
+        <Item> <img
+          src="https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg"
+          alt=""
+        /></Item>
+      </Grid>
+      <Grid item xs={'auto'}>
+        <Item> <img
+          src="https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg"
+          alt=""
+        /></Item>
+      </Grid>
       
     </React.Fragment>
   );
--- front/src/pages/home/Home.jsx
+++ front/src/pages/home/Home.jsx
@@ -9,7 +9,6 @@ const Home = () => {
     <div className="home">
       <Stories/>
       <Share/>
-      <Image_list/>
       <Posts/>
     </div>
   )
