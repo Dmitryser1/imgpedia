5b942f0018defbe76c4383299b535711b4170e79
Dmitryser1 <dmitryserafimovich@gmail.com>
2023-05-10 21:56:10 +0400

    123

--- front/src/components/Imagelist/Image_list.jsx
+++ front/src/components/Imagelist/Image_list.jsx
@@ -2,127 +2,339 @@ import * as React from 'react';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import ImageListItemBar from '@mui/material/ImageListItemBar';
+import ListSubheader from '@mui/material/ListSubheader';
 import IconButton from '@mui/material/IconButton';
-import StarBorderIcon from '@mui/icons-material/StarBorder';
+import InfoIcon from '@mui/icons-material/Info';
+import { colors } from '@mui/material';
 
-function srcset(image, width, height, rows = 1, cols = 1) {
-  return {
-    src: `${image}?w=${width * cols}&h=${height * rows}&fit=crop&auto=format`,
-    srcSet: `${image}?w=${width * cols}&h=${
-      height * rows
-    }&fit=crop&auto=format&dpr=2 2x`,
-  };
-}
-
-export default function CustomImageList() {
+export default function TitlebarImageList() {
   return (
-    <ImageList
-      sx={{
-        width: 1100,
-        height: 1000,
-        // Promote the list into its own layer in Chrome. This costs memory, but helps keeping high FPS.
-        transform: 'translateZ(0)',
-      }}
-      rowHeight={500}
-      gap={1}
-    >
-      {itemData.map((item) => {
-        const cols = item.featured ? 2 : 1;
-        const rows = item.featured ? 2 : 1;
+    <ImageList 
+      sx={{ width: 1, height: 1 }}
 
-        return (
-          <ImageListItem key={item.img} cols={cols} rows={rows}>
-            <img
-              {...srcset(item.img, 250, 200, rows, cols)}
-              alt={item.title}
-              loading="lazy"
-            />
-            <ImageListItemBar
-              sx={{
-                background:
-                  'linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, ' +
-                  'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
-              }}
-              title={item.title}
-              position="top"
-              actionIcon={
-                <IconButton
-                  sx={{ color: 'white' }}
-                  aria-label={`star ${item.title}`}
-                >
-                  <StarBorderIcon />
-                </IconButton>
-              }
-              actionPosition="left"
-            />
-          </ImageListItem>
-        );
-      })}
+    >
+      <ImageListItem  cols={10}>
+      </ImageListItem>
+      {itemData.map((item) => (
+        
+        <ImageListItem key={item.img}>
+          <img
+            src={`${item.img}?w=248&fit=crop&auto=format`}
+            srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+            alt={item.title}
+            loading="lazy"
+            onClick={"window.open(this.src)"}
+          />
+          <ImageListItemBar
+            sx={{background:'rgba(0,0,0,0)'}}
+            title={item.title}
+            subtitle={item.author}
+          />
+        </ImageListItem>
+      ))}
     </ImageList>
   );
 }
 
 const itemData = [
   {
-    img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-    title: 'Breakfast',
-    author: '@bkristastucchio',
-    featured: true,
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
   },
   {
-    img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-    title: 'Burger',
-    author: '@rollelflex_graphy726',
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
   },
   {
-    img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-    title: 'Camera',
-    author: '@helloimnik',
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
+  }, 
+  {
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
+  }, 
+  {
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
+  }, 
+  {
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
+  }, 
+  {
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
+  }, 
+  {
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
+  }, 
+  {
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
+  }, 
+  {
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
+  }, 
+  {
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
+  }, 
+  {
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
+  }, 
+  {
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
+  }, 
+  {
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
   },
   {
-    img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-    title: 'Coffee',
-    author: '@nolanissac',
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
   },
   {
-    img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
-    title: 'Hats',
-    author: '@hjrc33',
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
   },
   {
-    img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
-    title: 'Honey',
-    author: '@arwinneil',
-    featured: true,
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
   },
   {
-    img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
-    title: 'Basketball',
-    author: '@tjdragotta',
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
+  }, 
+  {
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
+  }, 
+  {
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
+  }, 
+  {
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
+  }, 
+  {
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
+  }, 
+  {
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
+  }, 
+  {
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
+  }, 
+  {
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
+  }, 
+  {
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
+  }, 
+  {
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
+  }, 
+  {
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
+  }, 
+  {
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
   },
   {
-    img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
-    title: 'Fern',
-    author: '@katie_wasserman',
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
   },
   {
-    img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
-    title: 'Mushrooms',
-    author: '@silverdalex',
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
   },
   {
-    img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-    title: 'Tomato basil',
-    author: '@shelleypauls',
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
   },
   {
-    img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-    title: 'Sea star',
-    author: '@peterlaster',
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
+  }, 
+  {
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
+  }, 
+  {
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
+  }, 
+  {
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
+  }, 
+  {
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
+  }, 
+  {
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
+  }, 
+  {
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
+  }, 
+  {
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
+  }, 
+  {
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
+  }, 
+  {
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
+  }, 
+  {
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
+  }, 
+  {
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
   },
   {
-    img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-    title: 'Bike',
-    author: '@southside_customs',
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
+  },
+  {
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
+  },
+  {
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
+  },
+  {
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
+  }, 
+  {
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
+  }, 
+  {
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
+  }, 
+  {
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
+  }, 
+  {
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
+  }, 
+  {
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
+  }, 
+  {
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
+  }, 
+  {
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
+  }, 
+  {
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
+  }, 
+  {
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
+  }, 
+  {
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
+  }, 
+  {
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
+  },
+  {
+    img: 'https://t3.ftcdn.net/jpg/03/45/05/92/240_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg',
+    title: 'Pic_name',
+    author: '@author',
   },
 ];
--- front/src/pages/home/Home.jsx
+++ front/src/pages/home/Home.jsx
@@ -3,13 +3,24 @@ import Posts from "../../components/posts/Posts"
 import Share from "../../components/share/Share"
 import "./home.scss"
 import Image_list from "../../components/Imagelist/Image_list"
+import {Avatar, Box } from '@mui/material';
 
 const Home = () => {
   return (
     <div className="home">
-      <Stories/>
+      <Image_list/>
       <Share/>
       <Posts/>
+      <Box
+        component="img"
+        sx={{
+          height: 1000,
+          width: 50,
+          
+        }}
+        alt="damn igor's pic."
+        src="https://sun7-18.userapi.com/impg/zALbsVUrFDmhK-emYSbkjEnxLASWsqvHT2PXqg/yDSTdlkoo-E.jpg?size=1600x900&quality=96&sign=2f7127f28bbbbf715b8175fdec3c9c7e&type=album"
+      />
     </div>
   )
 }
