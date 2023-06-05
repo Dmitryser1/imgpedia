968a78e7d1fee980763100bc8d428b047f6d9ee4
Dmitryser1 <dmitryserafimovich@gmail.com>
2023-06-05 21:11:17 +0400

    312313131

--- front/src/pages/home/Home.jsx
+++ front/src/pages/home/Home.jsx
@@ -3,7 +3,7 @@ import Posts from "../../components/posts/Posts"
 import Share from "../../components/share/Share"
 import "./home.scss"
 import {Avatar, Box } from '@mui/material';
-import React , {useContext} from 'react';
+import React , {useContext, useEffect} from 'react';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import ImageListItemBar from '@mui/material/ImageListItemBar';
@@ -13,6 +13,7 @@ import InfoIcon from '@mui/icons-material/Info';
 import { colors } from '@mui/material';
 import {observer} from 'mobx-react-lite';
 import { Context } from '../../index'
+import { getAllPhotos } from "../../http/AlbumAPI";
 
 function srcset(image, size, rows = 1, cols = 1) {
   return {
@@ -25,14 +26,29 @@ function srcset(image, size, rows = 1, cols = 1) {
 
 const Home =() => {
   const {image} = useContext(Context);
+  const KrutaiaFunction = async () => {
+    let data;
+    data = getAllPhotos(1)
+    console.log(data)
+    image.setImage(data)
+    console.log(image._photos.then())
+  };
+
+  useEffect(() => {
+    KrutaiaFunction()
+  }, [])
+
+
   return (
     <div className="home">
+      <h1>home</h1>
     <ImageList
       sx={{ width: 1, height: 1 }}
       variant="quilted"
       cols={8}
     >
       {image._photos.map(item => (
+        console.log(item)
         <ImageListItem key={item.img} cols={item.cols || 1} rows={item.rows || 1}>
           <img
             {...srcset(item.img, 120, item.rows, item.cols)}
@@ -41,7 +57,7 @@ const Home =() => {
           />
         </ImageListItem>
       ))}
-    </ImageList>
+      </ImageList>
     </div>
   )
 };
